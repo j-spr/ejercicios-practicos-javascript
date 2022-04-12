43c5c49f525570f33c147f8485990f2d1ceeadc0
Santiago Peñuela <rpenuela.js@gmail.com>
2022-04-12 12:39:36 -0500

    Agregando archivos Descuentos para segundo taller

---
+++ Descuentos/descuentos.js
@@ -0,0 +1,10 @@
+function calcularPrecioCondescuento(precio, descuento) {
+    const porcentaje_con_descuento = 100 - descuento;
+    const precio_con_descuento = (precio * porcentaje_con_descuento) / 100;
+    return precio_con_descuento
+}
+
+
+
+
+
